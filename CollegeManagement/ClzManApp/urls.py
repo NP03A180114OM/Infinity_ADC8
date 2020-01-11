ea4088d995c9ed94f44a7eee371ea88a2f9f1cdf
DahalPrakash <53021286+DahalPrakash@users.noreply.github.com>
2020-01-11 23:18:39 +0545

    Added Update Functionality

--- CollegeManagement/ClzManApp/urls.py
+++ CollegeManagement/ClzManApp/urls.py
@@ -3,4 +3,9 @@ from . views import *
 
 urlpatterns = [
     path('search/', search),
+    path('studentdata/',view_student_lists),
+    path('studentform/',view_student_form),
+    path('studentform/save',view_studentdata_save),
+    path('studentdata/edit/<int:ID>',view_studentdata_updateform),
+    path('studentdata/edit/update/<int:ID>',view_update_form_data_in_db),
 ]
--- CollegeManagement/ClzManApp/views.py
+++ CollegeManagement/ClzManApp/views.py
@@ -4,6 +4,8 @@ from django.http import HttpResponse, HttpResponseRedirect
 from django.contrib import messages
 
 # Create your views here.
+
+#For Searching
 def search(request):
     if request.method == "POST":
         search = request.POST['srh']
@@ -20,4 +22,57 @@ def search(request):
             return HttpResponse("<H1>Type the Student Name<H1>")
     else:
         return render(request, "search.html")
-        
+
+
+#for updating the database records
+def view_students_page(request):
+    return render(request,'student/student.html')
+
+def view_student_lists(request):
+    list_of_students= Students.objects.all()
+    print(list_of_students)
+    context_variable = {
+        'students':list_of_students
+    }
+    return render(request,'student/student.html',context_variable)
+
+def view_student_form(request):
+    return render(request,'student/studform.html')
+
+def view_studentdata_save(request):
+    if request.method == "POST":
+        get_all = request.POST
+        get_StudentID = request.POST['student_StudentID']
+        print(type(get_StudentID))
+        get_StudentName = request.POST['student_StudentName']
+        get_email = request.POST['student_email']
+        print(get_StudentID)
+        get_standard = request.POST['student_standard']
+        print(type(get_StudentID))
+        student_obj = Students(StudentID=get_StudentID,StudentName=get_StudentName,email=get_email,standard=get_standard)
+        student_obj.save()
+        return HttpResponse("<H1>Record Saved</H1>")
+    else:
+        return HttpResponse("Error record saving")
+
+def view_studentdata_updateform(request, ID):
+    print(ID)
+    student_obj = Students.objects.get(id=ID)
+    print(student_obj)
+    context_varible = {
+        'student':student_obj
+    }
+    return render(request,'student/studentupdateform.html',context_varible)
+
+def view_update_form_data_in_db(request, ID):
+    student_obj = Students.objects.get(id=ID)
+    print(student_obj)
+    student_form_data = request.POST
+    print(student_form_data)
+    student_obj.StudentID = request.POST['student_StudentID']
+    student_obj.StudentName =request.POST['student_StudentName']
+    student_obj.email = request.POST['student_email']
+    student_obj.standard = request.POST['student_standard']
+    student_obj.save()
+
+    return HttpResponse("<H1>Record Updated!!</H1>")
