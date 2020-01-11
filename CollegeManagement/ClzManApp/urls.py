4fe4d1048a91c58d81edebd4fa5b497930728317
NP03A180114OM <59682332+NP03A180114OM@users.noreply.github.com>
2020-01-11 23:37:29 +0545

    deleted update path

--- CollegeManagement/ClzManApp/urls.py
+++ CollegeManagement/ClzManApp/urls.py
@@ -7,14 +7,10 @@ urlpatterns = [
     path('studentdata/',view_student_lists),
     path('studentform/',view_student_form),
     path('studentform/save',view_studentdata_save),
-    path('studentdata/edit/<int:ID>',view_studentdata_updateform),
-    path('studentdata/edit/update/<int:ID>',view_update_form_data_in_db),
+   
     #for staff
     path('staffdata/',view_staff_lists),
     path('staffform/',view_staff_form),
     path('staffform/save',view_staffdata_save),
-    path('staffdata/edit/<int:ID>',view_staffdata_updateform),
-    path('staffdata/edit/update/<int:ID>',view_update_form_data_in_db),
-    #path('delete_staffdata/<str:TeacherID>',view_delete_staff,name="delete_Teacher"),
     path('staffdata/delete/<int:ID>',view_staff_delete), 
 ]
